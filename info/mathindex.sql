0d924a3acfbb6d0aeb15e5f40b7a43482907e4a2
Adrien - Leclère <adrien.leclere@orange.fr>
2024-04-08 16:42:38 +0200

    added all

---
+++ info/mathindex.sql
@@ -0,0 +1,213 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : jeu. 21 mars 2024 à 19:05
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `mathindex`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classroom`
+--
+
+CREATE TABLE `classroom` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `exercise`
+--
+
+CREATE TABLE `exercise` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `classroom_id` int(11) NOT NULL,
+  `thematic_id` int(11) NOT NULL,
+  `chapter` varchar(255) NOT NULL,
+  `keywords` text NOT NULL,
+  `difficulty` int(2) NOT NULL,
+  `duration` float NOT NULL,
+  `origin_id` int(11) NOT NULL,
+  `origin_name` varchar(255) NOT NULL,
+  `origin_information` text NOT NULL,
+  `exercice_file_id` int(11) NOT NULL,
+  `correction_file_id` int(11) NOT NULL,
+  `created_by_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `file`
+--
+
+CREATE TABLE `file` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `original_name` varchar(255) NOT NULL,
+  `extension` varchar(255) NOT NULL,
+  `size` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `origin`
+--
+
+CREATE TABLE `origin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `thematic`
+--
+
+CREATE TABLE `thematic` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `classroom`
+--
+ALTER TABLE `classroom`
+  ADD PRIMARY KEY (`id`,`name`);
+
+--
+-- Index pour la table `exercise`
+--
+ALTER TABLE `exercise`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `classroom_id` (`classroom_id`),
+  ADD KEY `thematic_id` (`thematic_id`),
+  ADD KEY `origin_id` (`origin_id`),
+  ADD KEY `exercice_file_id` (`exercice_file_id`),
+  ADD KEY `correction_file_id` (`correction_file_id`),
+  ADD KEY `created_by_id` (`created_by_id`);
+
+--
+-- Index pour la table `file`
+--
+ALTER TABLE `file`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `origin`
+--
+ALTER TABLE `origin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `thematic`
+--
+ALTER TABLE `thematic`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`,`email`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `classroom`
+--
+ALTER TABLE `classroom`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `exercise`
+--
+ALTER TABLE `exercise`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `file`
+--
+ALTER TABLE `file`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `origin`
+--
+ALTER TABLE `origin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `thematic`
+--
+ALTER TABLE `thematic`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `exercise`
+--
+ALTER TABLE `exercise`
+  ADD CONSTRAINT `exercise_ibfk_1` FOREIGN KEY (`classroom_id`) REFERENCES `classroom` (`id`),
+  ADD CONSTRAINT `exercise_ibfk_2` FOREIGN KEY (`thematic_id`) REFERENCES `thematic` (`id`),
+  ADD CONSTRAINT `exercise_ibfk_3` FOREIGN KEY (`origin_id`) REFERENCES `origin` (`id`),
+  ADD CONSTRAINT `exercise_ibfk_4` FOREIGN KEY (`exercice_file_id`) REFERENCES `file` (`id`),
+  ADD CONSTRAINT `exercise_ibfk_5` FOREIGN KEY (`correction_file_id`) REFERENCES `file` (`id`),
+  ADD CONSTRAINT `exercise_ibfk_6` FOREIGN KEY (`created_by_id`) REFERENCES `user` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
