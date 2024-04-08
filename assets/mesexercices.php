a592bdcbabc5aa41b8e2ebb48c2898a1b332b8cd
Adrien - Leclère <adrien.leclere@orange.fr>
2024-04-08 16:42:38 +0200

    added all

---
+++ assets/mesexercices.php
@@ -0,0 +1,17 @@
+<?php
+if(isset($_SESSION['email'])){
+    ?>
+    <div class="php_content">
+    
+    <div class="title_categ">Rechercher un exercice</div>
+        <div class="bloc_contenu2">
+    
+    
+    </div>
+    
+<?php
+}else{
+    echo'non, TG, Vous ne passez PASSSS!!!!';
+}
+
+?>
---
+++ connexion/logout.php
@@ -0,0 +1,10 @@
+<?php
+	// On récupère les variables de session
+	session_start();
+	
+    // Détruit toutes les variables de session --> fermeture de session
+    session_destroy();
+	
+    // Redirige vers la page de connexion
+    header("Location: ../index.php?accueil=1");
+?>
