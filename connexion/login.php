0f867353d6c39ff9012c9762dae31a32d5b3d479
emmanuel - rogue <emmanuelrogue.05@gmail.com>
2024-04-17 13:34:52 +0200

    j'ai modifier la conection , plus page de mdp

--- connexion/login.php
+++ connexion/login.php
@@ -1,4 +1,5 @@
 ﻿<link rel="stylesheet" href="style.css" />
+<link rel="stylesheet" href="style.css" />
 <?php
 require_once('connexion.php');
 $co = connexionBdd();
@@ -41,27 +42,23 @@ if (isset($_POST['submit'])) {
     <div class="title_categ">Connexion</div>
     
         
+    
+        
         <div class="bloc_contenu2">
+        <p class="texteconection">Cet espace est reservé aux enseignants du lycée Saint-Vincent-Senlis. Si vous n'avez pas encore de compte, veuillez effectuer votre demande directement 
+            en envoyant un mail à contact@lyceestvincent.net.  </p>
+        
         <p class="texteconection">Cet espace est reservé aux enseignants du lycée Saint-Vincent-Senlis. Si vous n'avez pas encore de compte, veuillez effectuer votre demande directement 
             en envoyant un mail à contact@lyceestvincent.net.  </p>
         
             <form class="box" action="#" method="post" name="login">
                 <h1 class="box-title">Connexion</h1>
                 <input type="mail" class="box-input" name="email" placeholder="Adresse mail" style="width: 30%; height: 60px;">
-                <input type="password" class="box-input" name="password" placeholder="Mot de passe" style="margin-top:1%; width: 30%; height: 60px;">
-                <input type="submit" value="Connexion " name="submit" class="box-button" style="
-                    border-radius: 5%;
-                    border: aliceblue;
-                    background-color: #f6f6f6;
-                    font-size: 99%;
-                    height: 37px;
-                    margin-top: 1%;
-                    width: 11%;
-                "></input>
-                
-
+                <input type="password" class="box-input" name="password" placeholder="Mot de passe" style="width: 30%; height: 60px;">
+                <input type="submit" value="Connexion " name="submit" class="box-button"></input>
                 <a href="?mdp_oublier=1">mot de passe oublier</a>
-                
+                <p class="box-register">Page d'accueil ? <a href="./index.php">Accueil</a></p>
+                <p class="box-register">S'inscrire ? <a href="connexion/register.php">ici</a></p>
                 <?php if (!empty($message)) { ?>
                     <p class="errorMessage"><?php echo $message; ?></p>
                 <?php } ?>
