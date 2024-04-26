1a5fbff413dc5a21b2557cecdf22dd60e79b205c
emmanuel - rogue <emmanuelrogue.05@gmail.com>
2024-04-26 12:00:29 +0200

    test soumettre fichier

--- assets/soumettre/fichiers.php
+++ assets/soumettre/fichiers.php
@@ -6,8 +6,10 @@
             $fichierEmpl = $_FILES['pdfExos']['tmp_name'] ; //Emplacement temporaire du fichier
             $fichierExtension = $_FILES['pdfExos']['type'] ; //type du fichier 
             $fichierTaille = $_FILES['pdfExos']['size'] ; //taille du fichier 
-            $emplacement = move_uploaded_file($fichierEmpl," ") ; 
-
+            $emplacement = move_uploaded_file($fichierEmpl,"C:/Math_Index_Groupe4/assets/administration/fichier/".$fichierNom) ; 
+            if($emplcement){ 
+                $chemin = "C:/Math_Index_Groupe4/assets/administration/fichier/".$fichierNom ; 
+            }
             $requete = $connexion->prepare("INSERT INTO file(`id`, `name`,`original_name`,`extension`,`size`)
             VALUES(Null, :nom, :chemin, :extension, :taille) ; ") ; 
         }
@@ -21,6 +23,27 @@
         <a href="?page=fichiers_soumettre"><p>Fichiers</p></a>
     </div>
         <div class="bloc_contenu2">
+            <h1><strong>Fichiers</strong></h1>
+            <br>
+            <form method = "POST" enctype = "multipart/form-data">
+                <label for = "pdfExos">Fiche exercice(PDF, WORD)* : </label>
+                <br>
+                <input type = "file" name  ="pdfExos" placeholder = "Séléctionner un fichier">
+                <br>
+<<<<<<< HEAD
+=======
+                <button name = "envoyer">Envoyer</button> 
+            </form> 
+            <h1><strong>Fichiers</strong></h1>
+            <br>
+            <form method = "POST" enctype = "multipart/form-data">
+                <label for = "pdfExos">Fiche corection(PDF, WORD)* : </label>
+                <br>
+                <input type = "file" name  ="pdfExos" placeholder = "Séléctionner un fichier">
+                <br>
+>>>>>>> 68f2c8a (correction de faute d'ortographe)
+                <button name = "envoyer"> 
+            </form> 
             <h1><strong>Fichiers</strong></h1>
             <br>
             <form method = "POST" enctype = "multipart/form-data">
