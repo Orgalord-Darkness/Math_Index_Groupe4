5e6d52e46a6985a7caca617eb92b71e0fda2b1da
emmanuel - rogue <emmanuelrogue.05@gmail.com>
2024-04-26 10:45:37 +0200

    Ajout de la requete insert into pour le pedf exos.

--- assets/soumettre/fichiers.php
+++ assets/soumettre/fichiers.php
@@ -1,15 +1,21 @@
-
 <?php
-    if(isset($_POT['envoyer'])){ 
+    if(isset($_POST['envoyer'])){ 
         if(isset($_FILES['pdfExos'])){ 
             $fichierNom = $_FILES['pdfExos']['name'] ; //Nom du fichier 
             $fichierEmpl = $_FILES['pdfExos']['tmp_name'] ; //Emplacement temporaire du fichier
             $fichierExtension = $_FILES['pdfExos']['type'] ; //type du fichier 
             $fichierTaille = $_FILES['pdfExos']['size'] ; //taille du fichier 
-            $emplacement = move_uploaded_file($fichierEmpl," ") ; 
-
+            $emplacement = move_uploaded_file($fichierEmpl,"C:/Math_Index_Groupe4/assets/administration/fichier/".$fichierNom) ; 
+            if($emplcement){ 
+                $chemin = "C:/Math_Index_Groupe4/assets/administration/fichier/".$fichierNom ; 
+            }
             $requete = $connexion->prepare("INSERT INTO file(`id`, `name`,`original_name`,`extension`,`size`)
             VALUES(Null, :nom, :chemin, :extension, :taille) ; ") ; 
+            $requete->bindParam(':nom',$fichierNom) ; 
+            $requete->bindParam(':chemin', $chemin) ;
+            $requete->bindParam(':extension', $fichierExtension) ; 
+            $requete->bindParam(':taille', $fichierTaille) ; 
+            $requete->execute() ;  
         }
     }
 ?>
@@ -21,6 +27,15 @@
         <a href="?page=fichiers_soumettre"><p>Fichiers</p></a>
     </div>
         <div class="bloc_contenu2">
+            <h1><strong>Fichiers</strong></h1>
+            <br>
+            <form method = "POST" enctype = "multipart/form-data">
+                <label for = "pdfExos">Fiche exercice(PDF, WORD)* : </label>
+                <br>
+                <input type = "file" name  ="pdfExos" placeholder = "Séléctionner un fichier">
+                <br>
+                <button name = "envoyer"> 
+            </form> 
             <h1><strong>Fichiers</strong></h1>
             <br>
             <form method = "POST" enctype = "multipart/form-data">
