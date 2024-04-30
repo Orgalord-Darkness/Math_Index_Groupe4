0fea23e6bdea8f517051b8d9b582767b3de55975
Orgalord-Darkness <heddy.mameri@gmail.com>
2024-04-30 18:48:09 +0200

    modification du filtre de formulaire via des fonctions

--- assets/administration/exercice/modif_exos.php
+++ assets/administration/exercice/modif_exos.php
@@ -1,122 +1,87 @@
 <?php
-  $id = 1 ; 
-    $erreurs = [];
-    if(isset($_POST['envoyer'])){ 
-          if(empty($_POST['nom_exercice'])){ 
-              $erreurs['nom'][] = "le champ nom doit-être renseigner " ; 
-          }
-          if(empty($_POST['classe'])){ 
-              $erreurs['classe'][] = "le champ classe doit-être renseigner " ; 
-          }
-          if(empty($_POST['thematique'])){ 
-              $erreurs['thematique'][] = "le champ thématique doit-être renseigner " ; 
-          }
-          if(empty($_POST['motscles'])){ 
-              $erreurs['motscles'][] = "le champ mots clés doit-être renseigner " ; 
-          }
-          if(empty($_POST['nchapitre'])){ 
-              $erreurs['nchapitre'][] = "le champ chapitre doit-être renseigner " ; 
-          }
-          if(empty($_POST['difficulte'])){ 
-              $erreurs['difficulte'][] = "le champ difficulté doit-être renseigner " ; 
-          }
-          if(empty($_POST['duree'])){ 
-              $erreurs['duree'][] = "le champ durée doit-être renseigner " ; 
-          }
-          if(empty($_POST['origine'])){ 
-              $erreurs['origine'][] = "le champ origine doit-être renseigner " ; 
-          }
-          if(empty($_FILES['pdfExos'])){ 
-              $erreurs['pdfExos'][] = "le champ fichier exercice doit-être renseigner " ; 
-          }
-          if(empty($_FILES['pdfCorrect'])){ 
-              $erreurs['pdfCorrect'][] = "le champ fichier correction doit-être renseigner " ; 
-          }
-          if(empty($_POST['idAuteur'])){ 
-              $erreurs['idAuteur'][] = "le champ auteur doit-être renseigner " ; 
-          }
-      }
-        if(empty($erreurs)){ 
-            if(isset($_POST['envoyer'])){ 
-                $nouvelle_date = $_POST['Ndate'];
-                $nouveau_nom = $_POST['nom_exercice'] ; 
-                // $nouvelle_matiere = $_POST['matiere'] ; 
-                $nouvelle_classe = $_POST['classe'] ;  
-                $nouvelle_thematique = $_POST['thematique']; 
-                $nouveau_nchapitre = $_POST['nchapitre'] ; 
-                $nouvelle_difficulte = $_POST['difficulte'] ; 
-                $nouvelle_duree = $_POST['duree'] ;
-                $nouveau_motscles = $_POST['motscles'] ; 
-                // $nouvelles_infos = $_POST['information'] ; 
-                // $pdf_exos = $_POST['pdf_exos'] ; 
-                // $pdf_correction = $_POST['pdf_correction'] ; 
-                $nouvelles_infos = "TESTTESTTEST"; 
-                $pdf_exos = 1 ; 
-                $pdf_correction = 1 ;
-                $origin_n = $_POST['origine'] ; 
-                $nouvelle_origine = $_POST['origine'] ;  
-                if($_POST['id_modif'] == null or $id ==null  ){ 
-                  $id = $_POST['id_manu'] ; 
-                }
-                $requete = $connexion->prepare("SELECT id FROM classroom WHERE name = :classname") ; 
-                $requete->bindParam(':classname',$nouvelle_classe) ; 
-                $test_class = $requete->execute() ;  
-                $id_class = $requete->fetchAll(PDO::FETCH_ASSOC) ;
-                $classe = implode(';', array_column($id_class, 'id'));
-
-                $requete = $connexion->prepare("SELECT id FROM thematic WHERE name = :thematicname") ; 
-                $requete->bindParam(':thematicname', $nouvelle_thematique) ; 
-                $test_thema = $requete->execute() ;  
-                $id_thematic = $requete->fetchAll(PDO::FETCH_ASSOC) ;    
-                $theme = implode(';', array_column($id_thematic, 'id')); 
-
-                $requete = $connexion->prepare("SELECT id FROM origin WHERE name = :originName") ; 
-                $requete->bindParam(':originName', $nouvelle_origine) ; 
-                $test_origin = $requete->execute() ;  
-                $id_origin = $requete->fetchAll(PDO::FETCH_ASSOC) ;    
-                $origin_id= implode(';', array_column($id_origin, 'id'));       
-                          // Connectez-vous à la base de données
-                          // echo "nouvelles infos extra : ".$nouveaun_nom.' '.$nouveau_prenom ; 
-                          // Préparez et exécutez la commande SQL pour la mise à jour
-                $requete = $connexion->prepare("UPDATE exercise SET name = :nom, classroom_id= :classe, thematic_id = :thematique,  chapter = :nchapitre,  keywords = :motscles, difficulty = :difficulte, duration = :duree, origin_id = :originId, origin_name = :originN, origin_information = :info, exercice_file_id = :pdfE, correction_file_id = :pdC, created_by_id = 1 
-                  WHERE id = :id");
-                $requete->bindParam(':id',$id , PDO::PARAM_INT);
-                // $requete->bindParam(':date_nouvelle', $nouvelle_date);
-                $requete->bindParam(':nom', $nouveau_nom);
-                // $requete->bindParam(':matiere', $nouvelle_matiere); 
-                $requete->bindParam(':classe', $classe, PDO::PARAM_INT);
-                $requete->bindParam(':thematique', $theme, PDO::PARAM_INT);
-                $requete->bindParam(':motscles', $nouveau_motscles) ; 
-                $requete->bindParam(':nchapitre', $nouveau_nchapitre);
-                $requete->bindParam(':difficulte', $nouvelle_difficulte) ;
-                $requete->bindParam(':duree', $nouvelle_duree) ; 
-                $requete->bindParam(':pdfE',$pdf_exos) ; 
-                $requete->bindParam(':pdC', $pdf_correction) ; 
-                $requete->bindParam(':info',$nouvelles_infos) ;  
-                $requete->bindParam(':originN', $origin_n) ; 
-                $requete->bindParam(':originId', $origin_id, PDO::PARAM_INT) ;
-
-                          
-              $resultat = $requete->execute();
-              var_dump($resultat) ; 
-
-                          // Vérifiez si la mise à jour a réussi
-              if ($resultat) {
-                echo "Les données ont été mises à jour avec succès.";
-              } else {
-                echo "Une erreur s'est produite lors de la mise à jour des données.";
-              }
-            }
-            else { 
-              echo "pas de modif " ; 
-            }
-          }else { 
-            echo "GET" ; 
-          
-            }
+$id = 1;
+$erreurs = [];
+$formulaire = [
+    'nom_exercice' => isset($_POST['nom_exercice']) ? $_POST['nom_exercice'] : "",
+    'classe' => isset($_POST['classe']) ? $_POST['classe'] : "",
+    'thematique' => isset($_POST['thematique']) ? $_POST['thematique'] : "",
+    'motscles' => isset($_POST['motscles']) ? $_POST['motscles'] : "",
+    'nchapitre' => isset($_POST['nchapitre']) ? $_POST['nchapitre'] : "",
+    'difficulte' => isset($_POST['difficulte']) ? $_POST['difficulte'] : "",
+    'duree' => isset($_POST['duree']) ? $_POST['duree'] : "",
+    'origine' => isset($_POST['origine']) ? $_POST['origine'] : "",
+    'pdfExos' => isset($_FILES['pdfExos']) ? $_FILES['pdfExos'] : "",
+    'pdfCorrect' => isset($_FILES['pdfCorrect']) ? $_FILES['pdfCorrect'] : "",
+    'idAuteur' => isset($_POST['idAuteur']) ? $_POST['idAuteur'] : "",
+];
+if(empty($erreurs)) {
+    if(isset($_POST['envoyer'])) {
+        $nouvelle_date = $_POST['Ndate'];
+        $nouveau_nom = $_POST['nom_exercice'];
+        $nouvelle_classe = $_POST['classe'];
+        $nouvelle_thematique = $_POST['thematique'];
+        $nouveau_nchapitre = $_POST['nchapitre'];
+        $nouvelle_difficulte = $_POST['difficulte'];
+        $nouvelle_duree = $_POST['duree'];
+        $nouveau_motscles = $_POST['motscles'];
+        $nouvelles_infos = "TESTTESTTEST";
+        $pdf_exos = 1;
+        $pdf_correction = 1;
+        $origin_n = $_POST['origine'];
+        $nouvelle_origine = $_POST['origine'];
+        if($_POST['id_modif'] == null or $id == null) {
+            $id = $_POST['id_manu'];
         }
+        $requete = $connexion->prepare("SELECT id FROM classroom WHERE name = :classname");
+        $requete->bindParam(':classname', $nouvelle_classe);
+        $test_class = $requete->execute();
+        $id_class = $requete->fetchAll(PDO::FETCH_ASSOC);
+        $classe = implode(';', array_column($id_class, 'id'));
+
+        $requete = $connexion->prepare("SELECT id FROM thematic WHERE name = :thematicname");
+        $requete->bindParam(':thematicname', $nouvelle_thematique);
+        $test_thema = $requete->execute();
+        $id_thematic = $requete->fetchAll(PDO::FETCH_ASSOC);
+        $theme = implode(';', array_column($id_thematic, 'id'));
+
+        $requete = $connexion->prepare("SELECT id FROM origin WHERE name = :originName");
+        $requete->bindParam(':originName', $nouvelle_origine);
+        $test_origin = $requete->execute();
+        $id_origin = $requete->fetchAll(PDO::FETCH_ASSOC);
+        $origin_id = implode(';', array_column($id_origin, 'id'));
+
+        $requete = $connexion->prepare("UPDATE exercise SET name = :nom, classroom_id= :classe, thematic_id = :thematique,  chapter = :nchapitre,  keywords = :motscles, difficulty = :difficulte, duration = :duree, origin_id = :originId, origin_name = :originN, origin_information = :info, exercice_file_id = :pdfE, correction_file_id = :pdC, created_by_id = 1 
+              WHERE id = :id");
+        $requete->bindParam(':id', $id, PDO::PARAM_INT);
+        $requete->bindParam(':nom', $nouveau_nom);
+        $requete->bindParam(':classe', $classe, PDO::PARAM_INT);
+        $requete->bindParam(':thematique', $theme, PDO::PARAM_INT);
+        $requete->bindParam(':motscles', $nouveau_motscles);
+        $requete->bindParam(':nchapitre', $nouveau_nchapitre);
+        $requete->bindParam(':difficulte', $nouvelle_difficulte);
+        $requete->bindParam(':duree', $nouvelle_duree);
+        $requete->bindParam(':pdfE', $pdf_exos);
+        $requete->bindParam(':pdC', $pdf_correction);
+        $requete->bindParam(':info', $nouvelles_infos);
+        $requete->bindParam(':originN', $origin_n);
+        $requete->bindParam(':originId', $origin_id, PDO::PARAM_INT);
+
+        $resultat = $requete->execute();
+        var_dump($resultat);
+
+        if ($resultat) {
+            echo "Les données ont été mises à jour avec succès.";
+        } else {
+            echo "Une erreur s'est produite lors de la mise à jour des données.";
+        }
+    } else {
+        echo "pas de modif ";
     }
+} else {
+    echo "GET";
+}
 ?>
+
 
     <h1 class = "titre_section">Administration</h1>
     <div class = "ajout_exos">
@@ -127,6 +92,11 @@
               <label for = "nom_exercice">Nom de l'exercice*</label>
               <br>
               <input type = "text" name = "nom_exercice" placeholder="Nom de l'exercice">
+              <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'nom_exercice', $_POST['nom_exercice']) ;
+                  }
+                ?>
               <br>
               <br>
               <label for = "matiere">Matière*</label>
@@ -145,6 +115,11 @@
                 <option value = "terminal">Terminal</option>
                 <option value = "Seconde2">Seconde2</option>
               </select>
+              <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'classe', $_POST['classe']) ;
+                  }
+                ?>
               <br>
               <br>
               <label for = "thematique">Thématique* : </label>
@@ -152,11 +127,21 @@
               <select name = "thematique">
                 <option value = "suite">Suite</option>
               </select>
+              <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'thematique', $_POST['thematique']) ;
+                  }
+                ?>
               <br>
               <br>
               <label for = "nchapitre">Numéro du chapitre : </label>
               <br>
               <input type = "text" name = "nchapitre">
+              <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'nchapitre', $_POST['nchapitre']) ;
+                  }
+                ?>
             </div>
             <div>
               <!-- <label for = "competence">Compétence</label>
@@ -180,11 +165,21 @@
               <label for = "motscles">Mots clés :</label>
               <br>
               <input name = "motscles" placeholer = "mots clés">
+              <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'motscles', $_POST['motscles']) ;
+                  }
+                ?>
               <br>
               <br>
               <label for = "origine">Origine : </label>
               <br>
               <input name = "origine">
+              <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'origine', $_POST['origine']) ;
+                  }
+                ?>
               <br>
               <label for = "difficulte">Difficultés :</label>
               <br>
@@ -201,16 +196,42 @@
                 <option value = "Niveau10">Niveau 10</option>
                 <option value = "Niveau11">Niveau 11</option>
               </select>
+              <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'difficulte', $_POST['difficulte']) ;
+                  }
+                ?>
               <br>
               <br>
               <label for = "duree">Durée</label>
               <br>
               <input type = "number" name = "duree">
+              <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'duree', $_POST['duree']) ;
+                  }
+                ?>
               <input name = "id_manu" placeholder = "id_modif">
             </div>
           </div>
           <br>
           <br>
+          <label for = 'pdfExos'>Fichier exercice : </label>
+                <br>
+                <input type = "file" name = "pdfExos" placeholder = "ID exos">
+                <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'pdfExos', $_FILES['pdfExos']) ;
+                  }
+                ?>
+                <label for = 'pdfCorrect'>Fichier de correction : </label>
+                <br>
+                <input type = "file" name = "pdfCorrect" placeholder = "IDCorrect">
+                <?php 
+                  if(isset($_POST['envoyer'])){ 
+                    addMessageIfValueEmpty($erreurs, 'pdfCorrect', $_FILES['pdfCorrect']) ;
+                  }
+                ?>
           <input type = "hidden" name = "id_modif" value = <?php echo $id?> >
           <button name = "envoyer">Continuer</button>
           <?php 
