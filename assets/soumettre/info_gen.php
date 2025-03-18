75aa6633f10d4575f252ed0d8605ef5b5cb4f894
Adrien <adrien.leclere@orange.fr>
2025-03-18 18:03:09 +0100

    UPDATE - Fix erreur mathindex + ajout de matière.

--- assets/soumettre/info_gen.php
+++ assets/soumettre/info_gen.php
@@ -5,6 +5,7 @@ function addMessageIfValueIsEmpty(array $errors, string $field): array
       if (empty($_POST[$field])) {
           $errors[$field][] = sprintf('Le champ "%s" doit être renseigné.', $field);
       }
+      return $errors;
   
 } 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['envoyer'])) {
@@ -40,16 +41,21 @@ function displayErrors(array $errors, string $field): void
                 <div>
                     <label for = "nom_exercice">Nom de l'exercice<span class="etoile">*</span> :</label>
                     <br>
-                    <input type = "text" name = "nom_exercice" id="nom_exercice" placeholder="Nom de l'exercice">
+                    <input type = "text" name = "nom_exercice" id="nom_exercice" placeholder="Nom de l'exercice...">
                     <?php displayErrors($errors, 'nom_exercice'); ?>
                     <br>
                     <br>
                     <label for = "matiere">Matière<span class="etoile">*</span> :</label>
                     <br>
                     <select name = "matiere" id="matiere">
-                        <option value=""></option>
-                        <option value= "mathematique"<?= (isset($informations['matiere']) && $informations['matiere'] == 'mathematique') ? 'selected' : '' ?>>Mathématique</option>
-                        <option value = "physique" <?= (isset($informations['matiere']) && $informations['matiere'] == 'physique') ? 'selected' : '' ?>>Physique</option>
+                        <option value="" disabled selected>Choisissez une matière...</option>
+                        <?php 
+                            $requete = $connexion->query("SELECT id, name FROM matter");
+                            $matieres = $requete->fetchAll(PDO::FETCH_ASSOC);
+                            foreach ($matieres as $matiere) {
+                                echo "<option value='".$matiere['id']."'>".$matiere['name']."</option>";
+                            }
+                        ?>
                     </select>
                     <?php displayErrors($errors, 'matiere'); ?>
                     <br>
@@ -57,23 +63,13 @@ function displayErrors(array $errors, string $field): void
                     <label for = "classe">Classe<span class="etoile">*</span> :</label>
                     <br>
                     <select name = "classe" id="classe">
+                        <option value="" disabled selected>Choisissez une classe...</option>
                     <?php 
-						$requete = $connexion->prepare("SELECT DISTINCT classroom_id FROM exercise");
-						$requete->execute();
-						$ids = $requete->fetchAll(PDO::FETCH_ASSOC);
-						$classes = [] ; 
-						foreach($ids as $id){ 
-							$requete_classe = $connexion->prepare("SELECT DISTINCT name FROM classroom WHERE id = :id") ; 
-							$requete_classe->bindParam(':id', $id['classroom_id'], PDO::PARAM_INT) ; 
-							$requete_classe->execute() ; 
-							$classe = $requete_classe->fetch(PDO::FETCH_ASSOC) ;  // Utilisez fetch() pour récupérer une seule ligne
-							if($classe) {  // Vérifiez si le résultat est non vide
-								 $classes[] = $classe['name'] ;  
-							}
-						}
-						for($ind = 0 ; $ind < count($classes) ; $ind++){ 
-						    echo "<option value='".$classes[$ind]."'>".$classes[$ind]."</option>"; 
-						}
+						$requete = $connexion->query("SELECT id, name FROM classroom");
+                        $classrooms = $requete->fetchAll(PDO::FETCH_ASSOC);
+                        foreach ($classrooms as $classroom) {
+                            echo "<option value='".$classroom['id']."'>".$classroom['name']."</option>";
+                        }
 					?>
                     </select>
                     <?php displayErrors($errors, 'classe'); ?>
@@ -82,22 +78,12 @@ function displayErrors(array $errors, string $field): void
                     <label for = "thematique">Thématique<span class="etoile">*</span> :</label>
                     <br>
                     <select name = "thematique" id="thematique">
+                        <option value="" disabled selected>Choisissez une thématique...</option>
                     <?php 
-                        $requete = $connexion->prepare("SELECT DISTINCT thematic_id FROM exercise");
-                        $requete->execute();
-                        $ids = $requete->fetchAll(PDO::FETCH_ASSOC);
-                        $themes = [] ; 
-                        foreach($ids as $id){ 
-                            $requete_theme = $connexion->prepare("SELECT DISTINCT name FROM thematic WHERE id = :id") ; 
-                            $requete_theme->bindParam(':id', $id['thematic_id'], PDO::PARAM_INT) ; 
-                            $requete_theme->execute() ; 
-                            $theme = $requete_theme->fetch(PDO::FETCH_ASSOC) ;  // Utilisez fetch() pour récupérer une seule ligne
-                            if($theme) {  // Vérifiez si le résultat est non vide
-                                $themes[] = $theme['name'] ;  
-                            }
-                        }
-                        for($ind = 0 ; $ind < count($themes) ; $ind++){ 
-                        echo "<option value='".$themes[$ind]."'>".$themes[$ind]."</option>"; 
+                        $requete = $connexion->query("SELECT id, name FROM thematic");
+                        $thematics = $requete->fetchAll(PDO::FETCH_ASSOC);
+                        foreach ($thematics as $thematic) {
+                            echo "<option value='".$thematic['id']."'>".$thematic['name']."</option>";
                         }
                     ?>
                     </select>
@@ -106,24 +92,24 @@ function displayErrors(array $errors, string $field): void
                     <br>
                     <label for = "nchapitre">Chapitre en cours :</label>
                     <br>
-                    <input type = "text" name = "nchapitre" id="nchapitre">
+                    <input type = "text" name = "nchapitre" id="nchapitre" placeholder="Choisissez le chapitre...">
                     <?php displayErrors($errors, 'nchapitre'); ?>
                 </div>
                 <div>
                     <label for = "motscles">Mots clés :</label>
-                    <br>
-                    <input name = "motscles" id="motscles" placeholer = "mots clés">
+                        <br>
+                    <input name="motscles" id="motscles" placeholder="Mots clés...">
                     <?php displayErrors($errors, 'motscles'); ?>
-                    <br>
+                        <br>
                     <label for = "info">Informations : </label>
-                    <br>
+                        <br>
                     <input name = 'info' placeholder = 'informations sur exercices'> 
-                    <br>
+                        <br>
                     <?php displayErrors($errors, 'info'); ?>
-                    <label for = "difficulte">Difficultés<span class="etoile">*</span> :</label>
-                    <br>
+                   <label for = "difficulte">Difficultés<span class="etoile">*</span> :</label>
+                        <br>
                     <select name = "difficulte" id="difficulte">
-                        <option value=""></option>
+                        <option value="" disabled selected>Choisissez une difficultée...</option>
                         <option value = "Niveau1">Niveau 1</option>
                         <option value = "Niveau2">Niveau 2</option>
                         <option value = "Niveau3">Niveau 3</option>
@@ -141,7 +127,7 @@ function displayErrors(array $errors, string $field): void
                     <br>
                     <label for = "duree">Durée</label>
                     <br>
-                    <input type = "number" name = "duree" id="duree">
+                    <input type = "number" name = "duree" id="duree" placeholder="Durée en heures...">
                     <?php displayErrors($errors, 'duree'); ?>
                 </div>
             </div>
--- assets/soumettre/source.php
+++ assets/soumettre/source.php
@@ -4,6 +4,7 @@ if(isset($_POST['nom_exercice'])){
     $nom_exos = $_POST['nom_exercice'] ; 
     $classe = $_POST['classe'] ; 
     $thematique = $_POST['thematique'] ; 
+    $thematique = $_POST['matiere'] ; 
     $nChapitre = $_POST['nchapitre'] ; 
     $motsCles = $_POST['motscles'] ; 
     $difficulte = $_POST['difficulte'] ; 
@@ -59,7 +60,7 @@ $origines = $requete->fetchAll(PDO::FETCH_ASSOC);
                     <label for="origine">Origine<span class="etoile">*</span> :</label>
                     <br>
                     <select name="origine" id="origine">
-                    <option value=""></option>
+                        <option value="" disabled selected>Choisissez une origine...</option>
                     <?php foreach ($origines as $origine) : ?>
                         <option value="<?= $origine['name'] ?>" <?= (isset($_POST['origine']) && $_POST['origine'] == $origine['name']) ? 'selected' : '' ?>>
                             <?= $origine['name'] ?>
@@ -72,7 +73,7 @@ $origines = $requete->fetchAll(PDO::FETCH_ASSOC);
                     <br>
                     <label for = "nom_source">Nom de la source/lien du site<span class="etoile">*</span> :</label>
                     <br>
-                    <input type = "text" name = "nom_source" id="nom_source" placeholder="Maths ">
+                    <input type = "text" name = "nom_source" id="nom_source" placeholder="Choisissez une source... ">
                     <?php displayErrors($errors, 'nom_source'); ?>
                     <br>
                     <br>
@@ -94,16 +95,6 @@ $origines = $requete->fetchAll(PDO::FETCH_ASSOC);
                     <button type="submit" name="envoyer">Continuer</button>
                 </div>
         </form>
-        <?php
-            // if(isset($nom_exos)){ 
-            //     var_dump($nom_exos) ; 
-            // }else{ 
-            //     echo "pas de exos"  ;
-            // }
-            // if(isset($_POST['envoyer'])){ 
-            //     var_dump($_POST['nom_exercice']) ;
-            // } 
-        ?>
     </div>     
 </div>
 <?php
